e9155b1e40ce61834d804c257f4bf7536ea44df9
Case Western Reserve University <andrew.mancuso@case.edu>
2018-03-11 16:50:49 -0400

    updated html css pseudocode initial commit

---
+++ assets/logic.js
@@ -0,0 +1,20 @@
+//form to input playername
+//waiting for another player to join
+//data goes to firebase, creates branches for choices
+//>multiRPS
+// -----------players
+// ------------------1
+// ---------------------choice - created on choice
+// ---------------------losses:0
+// ---------------------name:
+// ---------------------wins:0
+//
+// ------------------2
+// ---------------------choice - created on choice
+// ---------------------losses:0
+// ---------------------name:
+// ---------------------wins:0
+// >turn:1
+//display results
+//if disconnected removed from firebase
+//chat window where disconnect message is displayed
